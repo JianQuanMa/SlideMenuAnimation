8cff4f135319d244fcef1cb90436d462dcdcf02d
Jian Ma <jianquanma@gmail.com>
2019-12-09 18:47:32 -0500

    this is the first commit message

--- SlideMenuAnimation/ViewController.swift
+++ SlideMenuAnimation/ViewController.swift
@@ -5,14 +5,14 @@
 //  Created by Jian Ma on 12/9/19.
 //  Copyright © 2019 Jian Ma. All rights reserved.
 //
-
+//MARK: this project is meant to implement the animation of side menu sliding out like a fountain.
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
