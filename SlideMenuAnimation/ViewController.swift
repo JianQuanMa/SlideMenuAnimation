e1eed1a98ef21e2fd34dc9c0d41134645d8a9bde
Jian Ma <jianquanma@gmail.com>
2019-12-09 18:43:55 -0500

    Initial Commit

---
+++ SlideMenuAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideMenuAnimation
+//
+//  Created by Jian Ma on 12/9/19.
+//  Copyright © 2019 Jian Ma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
